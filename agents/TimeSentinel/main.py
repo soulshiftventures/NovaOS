4f31c8c90374510423c1301adb22beba625e4b56
soulshiftventures <soulshiftventures@gmail.com>
2025-07-20 01:12:30 -0500

    Switch to internal Key Value for TimeSentinel and DashboardAgent

--- agents/TimeSentinel/main.py
+++ agents/TimeSentinel/main.py
@@ -6,13 +6,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-REDIS_HOST = 'oregon-keyvalue.render.com'
+REDIS_HOST = 'red-d1u794c9c44c73cmjbf0'
 REDIS_PORT = 6379
-REDIS_USERNAME = 'red-d1u794c9c44c73cmjbf0'
-REDIS_PASSWORD = 'qzGkLOVr1Eyde9b6F0Mk87nYrOKFov4S'
 REDIS_DB = 0
 
-r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, username=REDIS_USERNAME, password=REDIS_PASSWORD, db=REDIS_DB, ssl=True, ssl_cert_reqs=None)
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
 
 def monitor_optimize():
     while True:
